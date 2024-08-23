5a23ea4ec07a6f605316ee63f245c9fe05f815ce
Sanmatiak <sanmatiak55@gmail.com>
2024-08-23 09:50:07 +0530

    Update battery_check.c

--- battery_check.c
+++ battery_check.c
@@ -31,7 +31,3 @@ int batteryIsOk(float temperature, float soc, float chargeRate) {
            isSocOk(soc) &&
            isChargeRateOk(chargeRate);
 }
-int main() {
-  assert(batteryIsOk(25, 70, 0.7));
-  assert(!batteryIsOk(50, 85, 0));
-}
